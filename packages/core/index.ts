282ed0054a9b24689198cf14c48eb72e90b071a9
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2023-10-04 15:14:49 -0300

    Set up core package config & export for testing

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,3 +1,16 @@
-function add(one: number, two: number) {
-	return one + two;
+import { TrackerThingConfigSchema, TrackerThingUserConfig } from './utils/config';
+
+export function createTracker(opts: TrackerThingUserConfig) {
+	const parsedConfig = TrackerThingConfigSchema.safeParse(opts);
+
+	if (!parsedConfig.success) {
+		throw new Error(
+			'Invalid configuration options passed to `@tracker-thing/core`\n' +
+				parsedConfig.error.issues.map((i) => i).join('\n')
+		);
+	}
+
+	const userConfig = parsedConfig.data;
+
+	console.log(userConfig);
 }
