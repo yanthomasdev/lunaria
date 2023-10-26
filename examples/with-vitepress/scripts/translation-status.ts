dda7915f9cd7628fc3f731eede05786ae88c4aca
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2023-10-26 18:28:42 -0300

    Use ts-node for Vitepress example

---
+++ examples/with-vitepress/scripts/translation-status.ts
@@ -0,0 +1,49 @@
+import { createTracker } from '@tracker-thing/core';
+
+const tracker = await createTracker({
+	repository: 'https://github.com/Yan-Thomas/tracker-thing',
+	rootDir: './examples/with-vitepress',
+	dashboard: {
+		//title: "Estado da Tradução",
+		url: 'https://localhost:3000/',
+		/* 		ui: {
+			lang: 'pt-BR',
+			"heading.progressByLanguage": "Progresso da tradução por língua",
+			"heading.statusByContent": "Estado da tradução por conteúdo",
+			"status.done": "completo",
+			"status.missing": "faltando",
+			"status.outdated": "desatualizado",
+			"status.emojiDone": "⭕",
+			"statusByContent.tableRowPage": "Página",
+			"statusByLanguage.outdatedTranslationLink": "tradução desatualizada",
+			"statusByLanguage.sourceChangeHistoryLink": "histórico de mudanças do original"
+
+		}, */
+	},
+	defaultLocale: {
+		label: 'English',
+		lang: 'en',
+		content: {
+			location: '**/*.md',
+			ignore: ['pt/*.md'],
+		},
+		dictionaries: {
+			location: 'ui/en/*.{js,cjs,mjs,ts,yml}',
+		},
+	},
+	locales: [
+		{
+			label: 'Português',
+			lang: 'pt',
+			content: {
+				location: 'pt/**/*.md',
+			},
+			dictionaries: {
+				location: 'ui/pt/*.{js,cjs,mjs,ts,yml}',
+			},
+		},
+	],
+	translatableProperty: 'i18nReady',
+});
+
+tracker.run();
