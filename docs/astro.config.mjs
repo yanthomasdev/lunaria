9487adbe5872f2457e2e49fb9ed48b4c944f03d1
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2023-11-06 22:29:04 -0300

    Initial `lunaria.dev` docs set up

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,9 +3,16 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://lunaria.dev",
 	integrations: [
 		starlight({
 			title: 'Lunaria',
+			social: {
+				github: "https://github.com/Yan-Thomas/lunaria"
+			},
+			editLink: {
+				baseUrl: "https://github.com/Yan-Thomas/lunaria/edit/main/docs/",
+			},
 			sidebar: [
 				{
 					label: 'Start Here',
