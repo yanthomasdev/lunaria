81e977909452cc69013c5bddee5e2ee94508eea2
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2023-11-08 20:40:57 -0300

    Add initial Lunaria brand assets

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,18 +1,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+const site = 'https://lunaria.dev/';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lunaria.dev',
+	site,
 	integrations: [
 		starlight({
 			title: 'Lunaria',
+			logo: {
+				light: "./src/assets/logo-light.svg",
+				dark: "./src/assets/logo-dark.svg",
+				replacesTitle: true,
+			},
 			social: {
 				github: 'https://github.com/Yan-Thomas/lunaria',
 			},
 			editLink: {
 				baseUrl: 'https://github.com/Yan-Thomas/lunaria/edit/main/docs/',
 			},
+			head: [
+				{
+					tag: 'meta',
+					attrs: { property: 'og:image', content: site + 'og.jpg?v=1' },
+				},
+				{
+					tag: 'meta',
+					attrs: { property: 'twitter:image', content: site + 'og.jpg?v=1' },
+				},
+			],
 			sidebar: [
 				{
 					label: 'Start Here',
