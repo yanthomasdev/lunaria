bfcefb18e2bd2dd9569a7fc9ea98102096db8fe6
Thomas Bnt <thomasbnt@protonmail.com>
2024-01-07 01:17:57 +0100

    i18n(fr): Init French translation, and locales in astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,12 +3,18 @@ import { defineConfig } from 'astro/config';
 
 const site = 'https://lunaria.dev/';
 
+export const locales = {
+	root: { label: 'English', lang: 'en' },
+	fr: { label: 'Français', lang: 'fr' },
+}
+
 // https://astro.build/config
 export default defineConfig({
 	site,
 	integrations: [
 		starlight({
 			title: 'Lunaria',
+			locales,
 			logo: {
 				light: './src/assets/logo-light.svg',
 				dark: './src/assets/logo-dark.svg',
@@ -33,14 +39,23 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Start Here',
+					translations: {
+						fr: 'Commencer ici',
+					},
 					items: [
 						{
 							label: 'Getting Started',
 							link: 'getting-started',
+							translations: {
+								fr: 'Bien démarrer',
+							}
 						},
 						{
 							label: 'Manual Installation',
 							link: 'manual-installation',
+							translations: {
+								fr: 'Installation manuelle',
+							}
 						},
 						/*{
 							label: 'Workflow',
