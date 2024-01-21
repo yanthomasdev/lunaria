162ab99b3fd7bd045af798545c3096a511e2eb15
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2024-01-21 19:42:59 -0300

    Add `@lunariajs/starlight` package

---
+++ packages/starlight/src/virtual.d.ts
@@ -0,0 +1,5 @@
+declare module 'virtual:lunaria-starlight' {
+	const LunariaStarlightConfig: import('./index').LunariaStarlightConfig;
+	export const pluginConfig: LunariaStarlightConfig;
+	export const isShallowRepo: boolean;
+}
