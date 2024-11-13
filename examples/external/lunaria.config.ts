f3e0577598c8d7d1cfebe1f89bae81db3767957c
Yan <61414485+yanthomasdev@users.noreply.github.com>
2024-11-13 20:06:12 -0300

    Update examples

--- examples/external/lunaria.config.ts
+++ examples/external/lunaria.config.ts
@@ -4,8 +4,106 @@ export default defineConfig({
 	repository: {
 		name: 'withastro/docs',
 	},
-	sourceLocale: 'en',
-	locales: ['ar', 'de', 'es', 'fr', 'hi', 'it', 'ja', 'ko', 'pl', 'pt-br', 'ru', 'zh-cn', 'zh-tw'],
+	sourceLocale: {
+		label: 'English',
+		lang: 'en',
+		parameters: {
+			tag: 'en',
+		},
+	},
+	locales: [
+		{
+			label: 'العربية',
+			lang: 'ar',
+			parameters: {
+				tag: 'ar',
+			},
+		},
+		{
+			label: 'Deutsch',
+			lang: 'de',
+			parameters: {
+				tag: 'de',
+			},
+		},
+		{
+			label: 'Español',
+			lang: 'es',
+			parameters: {
+				tag: 'es',
+			},
+		},
+		{
+			label: 'Français',
+			lang: 'fr',
+			parameters: {
+				tag: 'fr',
+			},
+		},
+		{
+			label: 'हिन्दी',
+			lang: 'hi',
+			parameters: {
+				tag: 'hi',
+			},
+		},
+		{
+			label: 'Italiano',
+			lang: 'it',
+			parameters: {
+				tag: 'it',
+			},
+		},
+		{
+			label: '日本語',
+			lang: 'ja',
+			parameters: {
+				tag: 'ja',
+			},
+		},
+		{
+			label: '한국어',
+			lang: 'ko',
+			parameters: {
+				tag: 'ko',
+			},
+		},
+		{
+			label: 'Polski',
+			lang: 'pl',
+			parameters: {
+				tag: 'pl',
+			},
+		},
+		{
+			label: 'Português do Brasil',
+			lang: 'pt-br',
+			parameters: {
+				tag: 'pt-BR',
+			},
+		},
+		{
+			label: 'Русский',
+			lang: 'ru',
+			parameters: {
+				tag: 'ru',
+			},
+		},
+		{
+			label: '简体中文',
+			lang: 'zh-cn',
+			parameters: {
+				tag: 'zh-CN',
+			},
+		},
+		{
+			label: '正體中文',
+			lang: 'zh-tw',
+			parameters: {
+				tag: 'zh-TW',
+			},
+		},
+	],
 	files: [
 		{
 			include: ['src/i18n/en/(ui|docsearch).ts'],
